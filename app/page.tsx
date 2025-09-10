24014df81f392cee5971b4b58df961a105183b2b
Victor Pinheiro Mello <victormello7003@hotmail.com>
2025-09-10 12:15:39 -0300

    feat: Adicionando header

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,12 @@ export default function Home() {
     <>
       <FixedThemeToggle />
       <Header />
+      <main className="bg-blue-600 w-screen h-screen"></main>
+      <main className="bg-blue-500 w-screen h-screen"></main>
+      <main className="bg-blue-100 w-screen h-screen"></main>
+      <main className="bg-blue-800 w-screen h-screen"></main>
+      <main className="bg-blue-300 w-screen h-screen"></main>
+      <footer id="about" className="bg-amber-50  h-screen w-screen"></footer>
     </>
   );
 }
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,25 +1,16 @@
+"use client";
+
 import { Separator } from "@/components/ui/separator";
+import { DrawerMenu } from "../drawer/drawerMenu";
 
 export function Header() {
   return (
-    <header className="flex justify-center items-center gap-3 overflow-hidden mt-5 px-5">
+    <header className="fixed w-screen flex justify-center items-center gap-3 overflow-x-hidden mt-5">
       <Separator
         orientation="horizontal"
         className="data-[orientation=horizontal]:w-[40%] bg-foreground"
       />
-
-      <div
-        className="bg-foreground hover:bg-background
-          text-card font-bold hover:text-card-foreground
-          flex items-center justify-center gap-2
-          border border-border rounded-full
-          transition ease-in
-          cursor-pointer
-          h-20 w-20"
-      >
-        MENU
-      </div>
-
+      <DrawerMenu />
       <Separator
         orientation="horizontal"
         className="data-[orientation=horizontal]:w-[40%] bg-foreground"
